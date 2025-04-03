593b9e5c4186a6dbe53cc3cd8cd32e76e5c3b767
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-04-03 23:04:57 +0200

    Revert "small adjusments"
    This reverts commit 3d6eb246a0160580b09e9cf1761b9ec368fe3fb2.

--- app/components/sections/home/FaqSection.vue
+++ app/components/sections/home/FaqSection.vue
@@ -13,7 +13,7 @@ const useStore = useIndexStore();
       <div
         v-for="(item, index) in useStore.faq.questions"
         :key="item.id"
-        class="flex gap-lg items-center border-b-2 border-highlight"
+        class="flex gap-lg items-center"
       >
         <span
           class="text-highlight font-bold text-heading-3xl"
